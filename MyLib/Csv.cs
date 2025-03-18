c8f44bc37bdd012be7a252e2ee3c6922e0882213
idekferewf <st50@WS50-09>
2025-03-18 13:22:35 +0300

    добавлен класс Csv & начато написание теста

---
+++ MyLib/Csv.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MyLib
+{
+    public class Csv
+    {
+        List<List<string>> data_ { get; set; }
+
+        public Csv(List<List<string>> data)
+        {
+            data_ = data;
+        }
+
+        public bool SaveToHtml(string outputPath)
+        {
+            // проверяем на существование данных
+            if (data_ == null || data_.Count == 0)
+            {
+                return false;
+            }
+
+            // начало html
+            string html = "<!DOCTYPE html><html><head><title>CSVData</title></head><body><table>";
+
+            // добавляем заголовки
+            html += "<thead><tr>";
+            foreach (string header in data_[0])
+            {
+                html += $"<th>{header}</th>";
+            }
+            html += "</tr></thead>";
+
+            // добавляем данные
+            html += "</tbody>";
+            for (int i = 1; i < data_.Count; i++)
+            {
+                html += "<tr>";
+                foreach (string cell in data_[i])
+                {
+                    html += $"<td>{cell}</td>";
+                }
+                html += "</tr>";
+            }
+            html += "</tbody>";
+
+            // конец html
+            html += "</table></tbody></html>";
+
+            // сохраняем файл
+            try
+            {
+                File.WriteAllText(outputPath, html, Encoding.UTF8);
+            } catch
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Testing/TCsv.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLib;
+
+namespace Testing
+{
+    [TestClass]
+    public class TCsv
+    {
+        [TestMethod]
+        public void TSaveToHtml()
+        {
+            // init
+            string outputPath = "D://Андреев Алексей";
+            List<List<string>> data = new List<List<string>>()
+            {
+                new List<string>() { "Имя", "Возраст" },
+                new List<string>() { "Артём", "18" },
+                new List<string>() { "Григорий", "17" },
+                new List<string>() { "Кирилл", "15" },
+            };
+
+            // сохраняем в HTML-файл
+            Csv csv = new Csv(data);
+            bool isSaved = csv.SaveToHtml(outputPath);
+        }
+    }
+}
