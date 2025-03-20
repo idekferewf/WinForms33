3475799118e60f05443373c1f292a06afb5c9d3f
idekferewf <example@gmail.com>
2025-03-20 11:01:11 +0300

    добавлен тест сохранения файла

--- Testing/TCsv.cs
+++ Testing/TCsv.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text;
 using MyLib;
 
 namespace Testing
@@ -12,7 +14,7 @@ namespace Testing
         public void TSaveToHtml()
         {
             // init
-            string outputPath = "D://Андреев Алексей";
+            string outputPath = "data.html";
             List<List<string>> data = new List<List<string>>()
             {
                 new List<string>() { "Имя", "Возраст" },
@@ -21,9 +23,29 @@ namespace Testing
                 new List<string>() { "Кирилл", "15" },
             };
 
-            // сохраняем в HTML-файл
             Csv csv = new Csv(data);
             bool isSaved = csv.SaveToHtml(outputPath);
+
+            // assert (is saved)
+            Assert.IsTrue(isSaved);
+
+            // actual
+            string actual = File.ReadAllText(outputPath, Encoding.UTF8);
+
+            // expected
+            string expected = "<!DOCTYPE html><html><head>" +
+                "<title>CSVData</title>" +
+                "</head>" +
+                "<body><table><thead>" +
+                "<tr><th>Имя</th><th>Возраст</th></tr>" +
+                "</thead><tbody>" +
+                "<tr><td>Артём</td><td>18</td></tr>" +
+                "<tr><td>Григорий</td><td>17</td></tr>" +
+                "<tr><td>Кирилл</td><td>15</td></tr>" +
+                "</tbody></table></tbody></html>";
+
+            // assert (html)
+            Assert.AreEqual(expected, actual);
         }
     }
 }
