e92118d96d2f6abf2b7542b65081d386c118f61d
idekferewf <example@gmail.com>
2025-03-20 12:54:53 +0300

    реализован интерфейс программы

--- WinForms33/MainForm.Designer.cs
+++ WinForms33/MainForm.Designer.cs
@@ -28,13 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.convertToHtmlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openCsvFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.saveHtmlFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.mainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(380, 24);
+            this.mainMenuStrip.TabIndex = 0;
+            this.mainMenuStrip.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadToolStripMenuItem,
+            this.convertToHtmlToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.loadToolStripMenuItem.Text = "Загрузить";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // convertToHtmlToolStripMenuItem
+            // 
+            this.convertToHtmlToolStripMenuItem.Enabled = false;
+            this.convertToHtmlToolStripMenuItem.Name = "convertToHtmlToolStripMenuItem";
+            this.convertToHtmlToolStripMenuItem.Size = new System.Drawing.Size(207, 22);
+            this.convertToHtmlToolStripMenuItem.Text = "Конвертировать в HTML";
+            this.convertToHtmlToolStripMenuItem.Click += new System.EventHandler(this.convertToHtmlToolStripMenuItem_Click);
+            // 
+            // openCsvFileDialog
+            // 
+            this.openCsvFileDialog.Filter = "CSV files (*.csv)|*.csv";
+            // 
+            // saveHtmlFileDialog
+            // 
+            this.saveHtmlFileDialog.DefaultExt = "html";
+            this.saveHtmlFileDialog.FileName = "data";
+            this.saveHtmlFileDialog.Filter = "HTML files (*.html)|*.html";
+            this.saveHtmlFileDialog.InitialDirectory = "./";
+            this.saveHtmlFileDialog.Title = "Сохранение";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(380, 226);
+            this.Controls.Add(this.mainMenuStrip);
+            this.MainMenuStrip = this.mainMenuStrip;
+            this.Name = "MainForm";
+            this.Text = "CSV to HTML converter";
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem convertToHtmlToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openCsvFileDialog;
+        private System.Windows.Forms.SaveFileDialog saveHtmlFileDialog;
     }
 }
 
